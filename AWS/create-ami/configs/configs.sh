111d2b74c71def9de2423afaef18520fb8a56e5b
kwsmkn <kwsmkn@users.noreply.github.com>
2017-06-19 18:05:00 +0900

    replace configs

---
+++ AWS/create-ami/configs/configs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CONFIG_DIR=/path/to/script/configs/
+CONFIG_BK_DIR=/path/to/script/configs/.backup/
+CONFIG_FILE="YOUR CONFIG FILE"
+
+OWNER_ID=876138579135
+NAMETAG="YOUR NAME TAG"
+AMINAME="TOUR AMI NAME"
